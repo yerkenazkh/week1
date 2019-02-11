48cb12c000cc6ebfd44883e334b32ba250e7eed9
Yerkenaz <khamidullayerkenaz@gmail.com>
2019-02-11 22:52:20 +0600

    lab1 tasks

---
+++ task2/task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year;
+        public Student()
+        {
+            name = Console.ReadLine();
+            id = Console.ReadLine();
+            year = Convert.ToInt32(Console.ReadLine());
+        }
+        public Student(string name, string id, int year)
+        {
+            this.name = name;
+            this.id = id;
+            this.year = year;
+        }
+        public void info()
+        {
+            Console.WriteLine(name + " " + id + " " + (year + 1));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student a = new Student();
+            a.info();
+            Student b = new Student("Yerkenaz", "18bd", 2018);
+            b.info();
+        }
+
+    }
+}
---
+++ task3/task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        public static int[] dublicate(int[] arr, int n)
+        {
+            int[] arr2 = new int[n * 2];
+            for (int i = 0, j = 0; i < n; i++)
+            {
+                arr2[j] = arr[i];
+                j++;
+                arr2[j] = arr[i];
+                j++;
+            }
+            return arr2;
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            string[] list = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(list[i]);
+
+            }
+            int[] arr2 = new int[n * 2];
+            arr2 = dublicate(arr, n);
+
+            for (int i = 0; i < n * 2; i++)
+            {
+                Console.Write(arr2[i] + " ");
+            }
+
+        }
+    }
+}
