d533ffac472cb8858b59ed321c9a8ed121c28e7b
yerkenazkh <47443502+yerkenazkh@users.noreply.github.com>
2019-02-15 00:34:38 +0600

    Update Program.cs

--- task1/task1/Program.cs
+++ task1/task1/Program.cs
@@ -10,18 +10,18 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            string[] s = Console.ReadLine().Split();
-            int[] a = new int[n];
+            int n = Convert.ToInt32(Console.ReadLine());//Convert string into int
+            string[] s = Console.ReadLine().Split();//read the line as a string and split it be space
+            int[] a = new int[n];//create a new array with integers
             for (int i = 0; i < n; i++)
             {
                 a[i] = Convert.ToInt32(s[i]);
-            }
-            int prime = 0;
-            int[] ans = new int[n];
+            }//this cycle need to place string into array
+            int prime = 0;//size of new array with integers
+            int[] ans = new int[n];//new array for prime numbers
             for (int i = 0; i < n; i++)
             {
-                int del = 0;
+                int del = 0;//counter for divider
                 for (int j = 1; j <= a[i]; j++)
                 {
                     if (a[i] % j == 0)
